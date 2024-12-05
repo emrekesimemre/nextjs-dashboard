896675a5c150b78726c3321f842c825e57c3dfb0
Emre KESIM <91027053+emrekesimemre@users.noreply.github.com>
2024-12-05 13:06:54 +0300

    feat: add some changes

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,7 +10,6 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
-import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
