688e1f0e7cc33dd77979f879a274d7a29b522d75
Emre KESIM <91027053+emrekesimemre@users.noreply.github.com>
2024-12-05 13:03:26 +0300

    feat: add some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+export const experimenral_ppr = true;
+
 export default function RootLayout({
   children,
 }: {
@@ -8,9 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={
-        `${inter.className} antialiased`
-      }>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
